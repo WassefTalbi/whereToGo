0a98f44d9cef2ba181393c1df98e12c706b860ef
aymenbrhm <aymen.benbrahim@esprit.tn>
2024-11-09 13:05:10 +0100

    Etablissement crud

---
+++ etablissement-service/src/main/java/com/esprit/etablissementservice/Controllers/EtablissementController.java
@@ -0,0 +1,41 @@
+package com.esprit.etablissementservice.Controllers;
+
+import com.esprit.etablissementservice.Entities.Etablissement;
+import com.esprit.etablissementservice.Services.EtablissementService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/etablissements")
+public class EtablissementController {
+    @Autowired
+    private EtablissementService etablissementService;
+
+    @GetMapping
+    public List<Etablissement> getAllEtablissements() {
+        return etablissementService.getAllEtablissements();
+    }
+
+    @GetMapping("/{id}")
+    public Optional<Etablissement> getEtablissementById(@PathVariable Long id) {
+        return etablissementService.getEtablissementById(id);
+    }
+
+    @PostMapping
+    public Etablissement createEtablissement(@RequestBody Etablissement etablissement) {
+        return etablissementService.createEtablissement(etablissement);
+    }
+
+    @PutMapping("/{id}")
+    public Etablissement updateEtablissement(@PathVariable Long id, @RequestBody Etablissement etablissement) {
+        return etablissementService.updateEtablissement(id, etablissement);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteEtablissement(@PathVariable Long id) {
+        etablissementService.deleteEtablissement(id);
+    }
+}
